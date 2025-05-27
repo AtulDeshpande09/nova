90037daee268950e878378e0fc462a12a4267068
AtulDeshpande09 <deshpandeatul05@gmail.com>
2025-05-27 15:35:25 +0530

    index page

--- app.py
+++ app.py
@@ -7,18 +7,25 @@ api_key = "52d129c124aaa073f4208583f3299b19"
 
 app = Flask(__name__)
 
+Global_location = None
+Global_weather_data = None
 
-
+# Main Index page
 @app.route("/")
 def index():
-    render_template("index.html")
+    return render_template("index.html")
 
+
+# Gives Weather information like
+# Temperature
+# Humidity
+# Rain in last 6 hours
 
 @app.route("/weather" , methods=['POST','GET'])
 def weather_data():
     weather_data = None
+
     if request.method=='POST':
-        print("IF statement")
         location = request.form['location']
         data = get_weather_info(location,api_key)
         
@@ -26,12 +33,32 @@ def weather_data():
         humidity  = data['main']['humidity']
         rain = data.get('rain',{}).get('6h',0)
 
+        Global_location = location
 
         weather_data = [("Temperature",temp),("Humidity",humidity),("Rain",rain)]
-        print(weather_data)
+        Global_weather_data = weather_data
         return render_template("weather.html" ,weather_data=weather_data)
 
     return render_template("weather.html",weather_data=weather_data)
+
+
+# Gives weather Based Advices
+@app.route("/advice",methods=['POST','GET'])
+def advice():
+    advice = None
+    if Global_location:
+        advice = "This is some advice"
+        return render_template("advice.html",advice=advice)
+    
+    location = None
+    return render_template('advice.html',advice=advice , location=location)
+
+
+@app.route("/fertilizer" , methods=["POST","GET"])
+def fertilizer():
+    return render_template("fertilizer.html")
+
+
 
 @app.route("/test",methods=["POST","GET"])
 def test():
